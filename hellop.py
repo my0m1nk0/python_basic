dc80c49ce34fe2814e3ff0fa0a052634e1eb4259
my0m1nk0 <myominko.msdev@gmail.com>
2025-01-09 20:23:49 +0630

    first commit

---
+++ hellop.py
@@ -0,0 +1,10 @@
+# file extension 
+# png jpg , pdf , docx , xls , mp3 ,mp4 ,exe 
+#.py #.java #class
+#.py
+# hello world.py # worng file name
+#test_folder , testFolder 
+# hello_world.py
+#python3 hellop.py
+
+print("Hello World")
